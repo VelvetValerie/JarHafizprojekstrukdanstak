243126afbf4f90b570619bb573fc8c7a8f9e41a1
Velvet <rafifadillah420@gmail.com>
2025-06-01 11:13:15 +0800

    Menambahkan fitur dasar struct

---
+++ C030324041_struct.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct Mahasiswa
+{
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
+};
+
+int main (int argc, char const *args[]){
+    
+}
