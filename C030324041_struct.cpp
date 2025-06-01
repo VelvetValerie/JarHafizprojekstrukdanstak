f81d3dc7cc97d99ec84993d7ffe869eb73b5a715
Velvet <rafifadillah420@gmail.com>
2025-06-01 11:08:54 +0800

    Menambahkan fitur full, empty, pop, push dan display

--- C030324041_struct.cpp
+++ C030324041_struct.cpp
@@ -34,3 +34,45 @@ int main(int argc, char const *argv[]){
 
     return 0;
 }
+
+void InitStack(Stack *S) {
+    S->Counts = 0;
+}
+
+int Full(Stack *S) {
+    return (S->Counts == MAXSTACK);
+}
+
+int Empty(Stack *S) {
+    return (S->Counts == 0);
+}
+
+void Pop(Stack *S, itemType *z) {
+    if (S->Counts == 0)
+    {
+        cout << "Stack masih kosong" << endl;
+    } else {
+        --(S->Counts);
+        *z = S->Item[S->Counts];
+    }
+    
+}
+
+void Push(Stack *S, itemType z) {
+    if (S->Counts == MAXSTACK) {
+        cout << "Stack sudah penuh" << endl;
+    } else {
+        S->Item[S->Counts] = z;
+        ++(S->Counts);
+    }
+}
+
+void display(Stack *S) {
+    cout << "Menampilkan data stack" << endl;
+
+    for (int i = (S->Counts) - 1; i >= 0; --i)
+    {
+        cout << S->Item[i] << endl;
+    }
+    
+}
