6d82609d753f6b83af814b549d0f7e0c8790e3e1
Velvet <rafifadillah420@gmail.com>
2025-06-01 10:50:09 +0800

    Menambahkan fitur maxstack

---
+++ C030324041_struct.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+typedef int itemType;
+
+const int MAXSTACK = 15;
+
+typedef struct stack{
+    itemType item[MAXSTACK];
+    int Counts;
+} Stack;
+
+int main(int argc, char const *argv[]){
+
+}
